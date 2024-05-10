329529e56f955f1cf93349cdb96d6af63d722b21
imre.gabor <imre.gabor@autsoft.hu>
2024-05-10 13:00:14 +0200

    use java from JAVA_HOME in local run script

--- local-run/app-start.ps1
+++ local-run/app-start.ps1
@@ -1,6 +1,6 @@
 .\mvnw package verify
 if ($LastExitCode -eq 0) {
-    java -Xmx512m "-Dserver.port=8082" `
+     & "$env:JAVA_HOME\bin\java" -Xmx512m "-Dserver.port=8082" `
     "-Dapi.message.url=http://localhost:8081" `
     -jar target\cloudnative-api-1.0.0-SNAPSHOT.jar
 }
